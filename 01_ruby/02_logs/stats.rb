c5a6ec51814f89a8fc92e97eff1ca7c9f683b624
sophiataskova <sophiataskova@yahoo.co.uk>
2012-12-13 09:23:48 -0500

    Update 01_ruby/02_logs/stats.rb
    Prints out all necessary information (most active page, day, user, etc). Some code is left commented out because I don't know why it didn't work.

--- 01_ruby/02_logs/stats.rb
+++ 01_ruby/02_logs/stats.rb
@@ -6,6 +6,9 @@ unless filename                         # we can't work without a filename
   exit
 end
 
+urls = Array.new
+users = Array.new
+dates = Array.new
 arrayOfVals = Array.new
 lines = 0                               # a humble line counter
 unique_users = 0                # someday, this will work
@@ -27,22 +30,39 @@ open(filename).each_with_index  do |m, i|
   lines += 1                            # bump the counter
 end
 
-h = Hash.new(0)
+h1 = Hash.new(0) # table of users
+h2 = Hash.new(0) # table of URLs
+h3 = Hash.new(0) # table of dates
 
-arrayOfVals.each { | v | h.store(v, h[v]+1) }
+urls = arrayOfVals.select.with_index{|_,i| (i+1) % 3 == 0}   # URLs are every 3rd element in the array of all values
+# urls.push arrayOfVals.delete(|i| (i+1) % 3 == 0)   # why doesn't this work?
 
-puts h
+#users = arrayOfVals.select.with_index{|_,i| (i+1) % 2 == 0}  # users are every other element in the array of all vals
+arrayOfVals.each { |x| users.push x if x.chars.first.eql?("U")}
+arrayOfVals.each { |x| dates.push x if x.chars.first.eql?("2")}
+
+users.each { | v | h1.store(v, h1[v]+1) }
+urls.each { | v | h2.store(v, h2[v]+1) }
+dates.each { | v | h3.store(v, h3[v]+1) }
+
+puts h1
+puts h2
+puts h3
+
 =begin
-h.keys.sort.each do |key|
-  puts key
-end
-=end
 h.keys.sort.each do |key|
   unique_users += 1 if key.chars.first.eql?("U")
   unique_pages += 1 if key.chars.first.eql?("/")
   unique_days += 1 if key.chars.first.eql?("2")
 end
+=end
 
+unique_users = h1.length
+unique_pages = h2.length
+
+most_active_user = h1.max_by{|k,v| v}
+most_active_page = h2.max_by{|k,v| v}
+most_active_day = h3.max_by{|k,v| v}
 
 
 puts "total lines: #{lines}"                  # output stats
